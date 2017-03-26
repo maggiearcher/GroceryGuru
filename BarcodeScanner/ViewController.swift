3c771d7faa4070d48ba140d38b5d3bee32b5e589
Amelia Delzell <adelzell@amelias-mbp.dhcp.wustl.edu>
2017-03-26 15:47:12 -0500

    Initial Commit

---
+++ BarcodeScanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BarcodeScanner
+//
+//  Created by Amelia Delzell on 3/26/17.
+//  Copyright © 2017 Amelia Delzell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
